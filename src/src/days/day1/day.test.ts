9e362bbde3c857aa92183fa6cbbfd063fc069e1f
William Cantin <william@wcantin.ca>
2024-12-01 12:44:32 -0500

    Done 1 done

--- src/src/days/day1/day.test.ts
+++ src/src/days/day1/day.test.ts
@@ -22,19 +22,19 @@ describe("Day1", () => {
 
   describe("Part2", () => {
     it("should return the correct answer for the example provided", () => {
-      const input = `two1nine
-      eightwothree
-      abcone2threexyz
-      xtwone3four
-      4nineeightseven2
-      zoneight234
-      7pqrstsixteen
+      const input = `
+      3   4
+      4   3
+      2   5
+      1   3
+      3   9
+      3   3
       `;
-      const expected = 281;
+      const expected = 31;
       expect(part2(input)).toBe(expected);
     });
     it("should return the correct answer for part 2", () => {
-      expect(part2(input)).toBe(54249);
+      expect(part2(input)).toBe(19678534);
     });
   });
 });
--- src/src/days/day1/day.ts
+++ src/src/days/day1/day.ts
@@ -3,24 +3,20 @@ const part1 = (input: string) => {
     .split("\n")
     .map((i) => i.trim())
     .filter((i) => i !== "");
-  console.log(inputs);
 
   const leftSideNumbers = inputs.map((i) => i.split("   ")[0]);
   const rightSideNumbers = inputs.map((i) => i.split("   ")[1]);
 
-  console.log(leftSideNumbers);
-  console.log(rightSideNumbers);
-
-  const leftOrderedBySmallest = leftSideNumbers.map(Number).sort((a, b) => a - b);
-  const rightOrderedBySmallest = rightSideNumbers.map(Number).sort((a, b) => a - b);
-
-  console.log(leftOrderedBySmallest);
-  console.log(rightOrderedBySmallest);
+  const leftOrderedBySmallest = leftSideNumbers
+    .map(Number)
+    .sort((a, b) => a - b);
+  const rightOrderedBySmallest = rightSideNumbers
+    .map(Number)
+    .sort((a, b) => a - b);
 
   const distanceBetweenLeftAndRight = leftOrderedBySmallest.map((i, index) => {
     return Math.abs(rightOrderedBySmallest[index] - i);
   });
-  console.log(distanceBetweenLeftAndRight);
 
   const answer = distanceBetweenLeftAndRight.reduce((acc, curr) => {
     return acc + curr;
@@ -34,7 +30,24 @@ const part2 = (input: string) => {
     .map((i) => i.trim())
     .filter((i) => i !== "");
 
-  const answer = "N/A";
+  const leftSideNumbers = inputs.map((i) => i.split("   ")[0]);
+  const rightSideNumbers = inputs.map((i) => i.split("   ")[1]);
+
+  const leftSideNumberOccurenceCountInRightSide = leftSideNumbers.map((i) => {
+    return rightSideNumbers.filter((j) => j === i).length;
+  });
+
+  console.log(leftSideNumberOccurenceCountInRightSide);
+
+  const similarityScore = leftSideNumbers.map((i, index) => {
+    return Number(i) * leftSideNumberOccurenceCountInRightSide[index];
+  });
+
+  console.log(similarityScore);
+
+  const answer = similarityScore.reduce((acc, curr) => {
+    return acc + curr;
+  });
   return answer;
 };
 

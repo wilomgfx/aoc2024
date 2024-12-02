b6747d7bcfcbd8c88138aeb1c12c298153bd7b86
William Cantin <wzc01@acuitysso.com>
2024-12-02 17:04:44 -0500

    Day 2 part 2 done

--- src/src/days/day2/day.test.ts
+++ src/src/days/day2/day.test.ts
@@ -22,19 +22,19 @@ describe("Day2", () => {
 
   describe("Part2", () => {
     it("should return the correct answer for the example provided", () => {
-      const input = `two1nine
-      eightwothree
-      abcone2threexyz
-      xtwone3four
-      4nineeightseven2
-      zoneight234
-      7pqrstsixteen
+      const input = `
+      7 6 4 2 1
+      1 2 7 8 9
+      9 7 6 2 1
+      1 3 2 4 5
+      8 6 4 4 1
+      1 3 6 7 9
       `;
-      const expected = 281;
+      const expected = 4;
       expect(part2(input)).toBe(expected);
     });
     it("should return the correct answer for part 2", () => {
-      expect(part2(input)).toBe(54249);
+      expect(part2(input)).toBe(476);
     });
   });
 });
--- src/src/days/day2/day.ts
+++ src/src/days/day2/day.ts
@@ -1,45 +1,45 @@
+const isIncreasing = (arr: number[]) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const isDecreasing = (arr: number[]) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const decreasingIncreasing = (arr: number[]) => {
+  return isDecreasing(arr) || isIncreasing(arr);
+};
+
+const adjacentDiffersByRange = (arr: number[]) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    const diff = Math.abs(arr[i] - arr[i + 1]);
+    if (diff < 1 || diff > 3) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const part1 = (input: string) => {
   const inputs = input
     .split("\n")
     .map((i) => i.trim())
     .filter((i) => i !== "");
 
-  const isIncreasing = (arr: number[]) => {
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  const isDecreasing = (arr: number[]) => {
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] < arr[i + 1]) {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  const decreasingIncreasing = (arr: number[]) => {
-    return isDecreasing(arr) || isIncreasing(arr);
-  };
-
   const decreasingIncreasingRows = inputs.map((row) => {
     const numbers = row.split(" ").map((n) => parseInt(n));
     return decreasingIncreasing(numbers);
   });
-
-  const adjacentDiffersByRange = (arr: number[]) => {
-    for (let i = 0; i < arr.length - 1; i++) {
-      const diff = Math.abs(arr[i] - arr[i + 1]);
-      if (diff < 1 || diff > 3) {
-        return false;
-      }
-    }
-    return true;
-  };
 
   const adjacentDiffersByRangeRows = inputs.map((row) => {
     const numbers = row.split(" ").map((n) => parseInt(n));
@@ -59,7 +59,42 @@ const part2 = (input: string) => {
     .map((i) => i.trim())
     .filter((i) => i !== "");
 
-  const answer = "N/A";
+  const decreasingIncreasingRows = inputs.map((row) => {
+    const numbers = row.split(" ").map((n) => parseInt(n));
+    return decreasingIncreasing(numbers);
+  });
+
+  const adjacentDiffersByRangeRows = inputs.map((row) => {
+    const numbers = row.split(" ").map((n) => parseInt(n));
+    return adjacentDiffersByRange(numbers);
+  });
+
+  const isSafeAfterRemovingOne = (arr: number[]) => {
+    for (let i = 0; i < arr.length; i++) {
+      const newArr = arr.slice(0, i).concat(arr.slice(i + 1));
+      if (adjacentDiffersByRange(newArr) && decreasingIncreasing(newArr)) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const rowsMatchingBothRules = decreasingIncreasingRows.map(
+    (dir, i) => dir && adjacentDiffersByRangeRows[i]
+  );
+  const unsafeRows = rowsMatchingBothRules.map((isSafe, i) => {
+    if (!isSafe) {
+      const numbers = inputs[i].split(" ").map((n) => parseInt(n));
+      if (isSafeAfterRemovingOne(numbers)) {
+        //"unsafe but can be safe by removing one number"
+        return "safe";
+      }
+      return "unsafe";
+    }
+    return "safe";
+  });
+
+  const answer = unsafeRows.filter((i) => i === "safe").length;
   return answer;
 };
 

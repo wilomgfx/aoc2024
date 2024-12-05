04825915545a8cbccfaf880d2e8ac6f2bba8fdac
William Cantin <wzc01@acuitysso.com>
2024-12-05 10:52:10 -0500

    Day 5 part 1 done

---
+++ src/src/days/day5/day.test.ts
@@ -0,0 +1,62 @@
+import { expect, it, describe } from "vitest";
+import { part1, part2 } from "./day";
+import input from "./input";
+describe("Day5", () => {
+  describe("Part1", () => {
+    it("should return the correct answer for the example provided", () => {
+      const input = `
+      47|53
+      97|13
+      97|61
+      97|47
+      75|29
+      61|13
+      75|53
+      29|13
+      97|29
+      53|29
+      61|53
+      97|53
+      61|29
+      47|13
+      75|47
+      97|75
+      47|61
+      75|61
+      47|29
+      75|13
+      53|13
+
+      75,47,61,53,29
+      97,61,53,29,13
+      75,29,13
+      75,97,47,61,53
+      61,13,29
+      97,13,75,29,47
+      `;
+      const expected = 143;
+      expect(part1(input)).toBe(expected);
+    });
+    it("should return the correct answer for part 1", () => {
+      expect(part1(input)).toBe(5509);
+    });
+  });
+
+  describe("Part2", () => {
+    it("should return the correct answer for the example provided", () => {
+      const input = `two1nine
+      eightwothree
+      abcone2threexyz
+      xtwone3four
+      4nineeightseven2
+      zoneight234
+      7pqrstsixteen
+      `;
+      const expected = 281;
+      expect(part2(input)).toBe(expected);
+    });
+    it("should return the correct answer for part 2", () => {
+      expect(part2(input)).toBe(54249);
+    });
+  });
+});
---
+++ src/src/days/day5/day.ts
@@ -0,0 +1,60 @@
+const part1 = (input: string) => {
+  const [orderRulesSection, pageUpdatesSection] = input.split("\n\n");
+  const orderRules = orderRulesSection
+    .split("\n")
+    .map((rule) => rule.trim())
+    .filter((rule) => rule !== "");
+  const pageUpdates = pageUpdatesSection
+    .split("\n")
+    .map((update) => update.trim())
+    .filter((update) => update !== "");
+
+  const pageUpdateSatisfyOrderRule = (
+    pageUpdate: string,
+    orderRules: string[]
+  ) => {
+    const pages = pageUpdate.split(",").map((page) => page.trim());
+
+    for (const rule of orderRules) {
+      const [before, after] = rule.split("|").map(Number);
+      const beforeIndex = pages.indexOf(before.toString());
+      const afterIndex = pages.indexOf(after.toString());
+
+      if (beforeIndex === -1 || afterIndex === -1) {
+        // If either page is not in the update, skip this rule
+        continue;
+      }
+
+      if (beforeIndex >= afterIndex) {
+        // If the 'before' page comes after the 'after' page, the rule is violated
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  const validPageUpdates = pageUpdates.filter((update) =>
+    pageUpdateSatisfyOrderRule(update, orderRules)
+  );
+
+  const middlePages = validPageUpdates.map((update) => {
+    const pages = update.split(",").map(Number);
+    return pages[Math.floor(pages.length / 2)];
+  });
+
+  const answer = middlePages.reduce((acc, page) => acc + page, 0);
+  return answer;
+};
+
+const part2 = (input: string) => {
+  const inputs = input
+    .split("\n")
+    .map((i) => i.trim())
+    .filter((i) => i !== "");
+
+  const answer = "N/A";
+  return answer;
+};
+
+export { part1, part2 };
